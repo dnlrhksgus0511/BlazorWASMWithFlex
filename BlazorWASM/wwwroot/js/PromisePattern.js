f1376eefb18753c0dba6eaaca64fe9368cef3bfe
wigwanhyun <dnlrhksgus0511@gmail.com>
2020-07-03 09:26:53 +0900

    Generic 적용

--- BlazorWASM/wwwroot/js/PromisePattern.js
+++ BlazorWASM/wwwroot/js/PromisePattern.js
@@ -1,14 +1,14 @@
-﻿function WithPromise(promiseHandler) {
-    return new Promise((resolve, reject) => {
-        console.log('Initial');
-        resolve();
-    }).then((val) => {
-        sleep(10000);
-        return { "key": "dddd" };
-    });
-}
+﻿//function WithPromise(promiseHandler) {
+//    return new Promise((resolve, reject) => {
+//        console.log('Initial');
+//        resolve();
+//    }).then((val) => {
+//        sleep(10000);
+//        return { "key": "dddd" };
+//    });
+//}
 
-async function WithAsync(promiseHandler) {
+async function WithAsync() {
     await new Promise((resolve, reject) => {
         console.log('Initial');
         sleep(10000);
@@ -17,15 +17,11 @@ async function WithAsync(promiseHandler) {
     return { "key": "dddd" };
 }
 
-async function foo(promiseHandler) {
-    const res = await WithPromise();
+async function CallPromise(promiseHandler) {
+    const res = await WithAsync();
     //결과를 C# 단에 있는 SetResult를 호출
-    promiseHandler.invokeMethodAsync('SetResult', JSON.stringify(res));
-}
-
-async function camera(promiseHandler) {
-    const res = await $flex.camera();
-    //{ auth:"", data: "" }
+    promiseHandler.invokeMethodAsync('SetResult', res);
+    //promiseHandler.invokeMethodAsync('SetResult', JSON.stringify(res));
 }
 
 function sleep(delay) {
